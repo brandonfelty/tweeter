faddbd4773ad2cf048cbd29f28ac50a244e053c6
Brandon Felty <brcfelty@gmail.com>
2022-04-26 21:18:34 -0600

    created a function loadTweets that retrieves the array of tweets from the database and renders them in the app

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,6 @@
 // Function takes in a tweet object and returns tweet <article> element in html
 
 $( () => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
   
   const renderTweets = function(tweets) {
 
@@ -81,15 +57,21 @@ $( () => {
     // return tweet
     return article;
   };
-  
-  renderTweets(data);
 
-  
+  // Event listener.. Checks for form click and then posts the serialized form data to the server
   $("form").submit( function(event) {
     event.preventDefault();
-    console.log($(this).serialize());
     const serializedFormData = $(this).serialize();
     const url = $(this).attr("action");
     $.post(url, serializedFormData);
   })
+
+  // function that retrieves an array of tweets from the database
+  const loadTweets = function() {
+    $.get("/tweets", function(data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 });
