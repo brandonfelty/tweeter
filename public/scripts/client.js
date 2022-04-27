3ddcdb3841cd89094920435ce8daa895b29ddd94
Brandon Felty <brcfelty@gmail.com>
2022-04-27 14:33:20 -0600

    changed my renderTweets function so that it emptied the tweet container. Also added it as a callback function to the form submit post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,9 +6,11 @@
 
 // Function takes in a tweet object and returns tweet <article> element in html
 
-$( () => {
-  
-  const renderTweets = function(tweets) {
+$(() => {
+
+  const renderTweets = function (tweets) {
+    const container = $('#tweets-container');
+    container.empty();
 
     // loops through tweets
     for (const tweet of tweets) {
@@ -17,20 +19,20 @@ $( () => {
       const $tweet = createTweetElement(tweet);
 
       // takes return value and appends it to the tweets container
-      $('#tweets-container').prepend($tweet);
+      container.prepend($tweet);
     }
-  }
+  };
 
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
 
-    
+
     // get info for article
     const img = tweet.user.avatars;
     const username = tweet.user.name;
     const handle = tweet.user.handle;
     const text = tweet.content.text;
     const time = timeago.format(tweet.created_at);
-    
+
     // Function to escape text 
     const escape = function (str) {
       let div = document.createElement("div");
@@ -62,13 +64,13 @@ $( () => {
         </footer>
       </article>
     `;
-    
+
     // return tweet
     return article;
   };
 
   // Event listener.. Checks for form click and then posts the serialized form data to the server
-  $("form").submit( function(event) {
+  $("form").submit(function (event) {
     $('.error').slideUp("slow");
     event.preventDefault();
     const serializedFormData = $(this).serialize();
@@ -80,7 +82,7 @@ $( () => {
       $('.error').slideUp("slow");
       $('.error').slideDown("slow");
       //$('.error').css('display', 'block');
-      return $('.error').html('If you tweet, tweet something!')
+      return $('.error').html('If you tweet, tweet something!');
       // return alert('If you tweet, tweet something!');
     }
 
@@ -89,21 +91,22 @@ $( () => {
       //$('.error').css('display', 'block');
       $('.error').slideUp("slow");
       $('.validation').slideDown("slow");
-      return $('.validation').html('Please shorten your tweet')
+      return $('.validation').html('Please shorten your tweet');
       // return alert('Please shorten your tweet');
     }
 
     $.post(url, serializedFormData, () => {
+      loadTweets();
     });
-
-    //location.reload();
-  })
+  });
 
   // function that retrieves an array of tweets from the database and renders them in the app
-  const loadTweets = function() {
-    $.get("/tweets", function(data) {
-      renderTweets(data);
-    });
+  const loadTweets = function () {
+    $.get("/tweets")
+      .then((data) => {
+        renderTweets(data);
+      }
+      );
   };
 
   loadTweets();
